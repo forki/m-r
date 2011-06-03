4dd3c40550998335925f92ae5403a403455fea36
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2011-06-03 20:38:59 +0200

    Small cleanup

--- SimpleCQRS.FS/Messages.fs
+++ SimpleCQRS.FS/Messages.fs
@@ -1,14 +1,10 @@
 ﻿module Messages
 
-type Message = interface end
-
 type 'a Command = { CommandData: 'a }
-    with interface Message
 
 type 'a Event = {
     EventData: 'a
     Version: int }
-    with interface Message
 
 let toCommand x = { CommandData = x } : 'a Command
 let toEvent x = { EventData = x; Version = 0 } 
